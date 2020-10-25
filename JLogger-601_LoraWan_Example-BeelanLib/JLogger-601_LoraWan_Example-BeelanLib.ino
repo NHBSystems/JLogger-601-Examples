275eee01ed3cde5e4ff2c0f54bd10dbb73afda41
Jaimy Juliano <jjuliano77@gmail.com>
2020-10-25 15:35:05 -0500

    Add hardware test and LoraWan (BeeLan lib) example

---
+++ JLogger-601_LoraWan_Example-BeelanLib/JLogger-601_LoraWan_Example-BeelanLib.ino
@@ -0,0 +1,494 @@
+/*************************************************************************************
+ * JLogger 601 example using Beelan-LoRaWAN library with AD7794 and low power 
+ * sleep (~14 uA)
+ * 
+ * Sends readings (battery voltage and 6 ADC channels + temperature from the
+ * AD7794) over LoRaWAN every 60 seconds, sleeping in between. 
+ * 
+ * Includes an example of downlink command that changes the Tx/sleep interval
+ * 
+ * The required Javascript uplink Decode and downlink Encode functions are  
+ * included in comment at the end of the sketch. You will need to copy and 
+ * paste into Chirpstack (or TTN, whatever).
+ * 
+ * Written for and tested with Chirpstack, will work with minimal changes to the
+ * function names and signature on TTN or some other network 
+ * 
+ * Tested with: 
+ *  1.3.0 (Modified - SPI transaction mod)
+ *  1.5.1
+ * 
+ ************************************************************************************/
+
+
+
+#include <lorawan.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <RTCZero.h>
+#include "NHB_AD7794.h"
+#include <extEEPROM.h>
+
+
+#define SERIAL_DEBUG
+
+#define MIN_INTERVAL        10     //Minimum sleep/transmit interval
+#define MAX_INTERVAL        3600   //Maximum sleep/transmit interval
+
+#define EUI64_I2C_ADDR      0x50   //Not currently used, but left here for reference
+#define EUI64_MAC_ADDR      0xF8
+#define EUI64_MAC_LENGTH    0x08
+
+#define DLCMD_SET_RATE      1
+//#define DLCMD_OTHERCMD      2
+
+
+// OTAA credentials
+char devEui[16];  //Will fill this with EUI from 24AA025E64 chip
+const char *appEui = "0000000000000000"; //Chirpstack doesnt use this
+const char *appKey = "93aa30ad90364fa818d311e7d88e9aef"; 
+
+
+//Tx/sleep timing
+uint16_t sleepInterval = 60; 
+uint8_t nextAlarmSec;
+uint8_t nextAlarmMin;
+
+
+//Pin mapping for JLogger 601
+const sRFM_pins RFM_pins = {
+  .CS = PIN_RFM_CS, //2,
+  .RST = PIN_RFM_RST, //27,
+  .DIO0 = PIN_RFM_DIO_0, //7,
+  .DIO1 = PIN_RFM_DIO_1, //38,
+  .DIO2 = 255, //Not connected
+  .DIO5 = 255, //Not connected
+};
+
+
+AD7794 adc(PIN_AD7794_CS,4000000, 2.50);    // Create the ADC object
+
+RTCZero rtc;
+
+extEEPROM eep(kbits_2, 1, 16);  //device size, number of devices, page size
+
+uint8_t eui[8];
+
+
+/////////////////////////// Setup //////////////////////////////////////////////
+
+void setup() {
+  asm(".global _printf_float"); // Enable sprintf float support
+  
+  //delay(2000);
+
+  #ifdef SERIAL_DEBUG
+    while (! Serial);
+    Serial.begin(115200);
+  #endif  
+
+//  initLoggerPins();
+
+  analogReadResolution(12);
+
+  //Initialize the 24AA025E64 
+  uint8_t eepStatus = eep.begin(eep.twiClock400kHz);   //go fast!
+  if (eepStatus) {
+    #ifdef SERIAL_DEBUG
+      Serial.print(F("extEEPROM.begin() failed, status = "));
+      Serial.println(eepStatus);
+    //while (1);
+    #endif
+  }
+
+  //Read the EUI
+  eep.read(EUI64_MAC_ADDR, eui, EUI64_MAC_LENGTH); 
+
+  //...And convert it to a HEX string
+  char buf[3];
+  for(int i=0;i<8;i++){
+    sprintf(buf, "%02x",eui[i]);   
+    devEui[i*2] = buf[0];
+    devEui[i*2 + 1] = buf[1];
+  }
+  devEui[16] = 0; //NULL Termination
+
+  #ifdef SERIAL_DEBUG
+    Serial.print(F("devEUI: "));
+    Serial.println(devEui);
+  #endif
+
+
+  //Initialize RTC
+  rtc.begin();
+
+  //Set time actual time here if needed
+
+  nextAlarmSec = rtc.getSeconds();
+  nextAlarmMin = rtc.getMinutes();
+
+
+  //Initialize the AD7794
+  adc.begin();
+  adc.setUpdateRate(16.6); //< Slow, but 16.6Hz rate enables 50/60hz noise rejection
+  for(int i=0; i < 6; i++){
+    adc.setBipolar(i,true);
+    //delay(2);
+    adc.setGain(i, 128);
+    //delay(2);
+    adc.setEnabled(i,true);
+    //delay(2);
+  }
+
+
+
+  //Initialize radio and LoRaWAN
+  if(!lora.init()){
+    #ifdef SERIAL_DEBUG
+      Serial.println("RFM95 not detected");
+    #endif
+    delay(1000);
+    while(1){
+      //Just freak out
+      digitalWrite(LED_BUILTIN,LOW);
+      delay(500);
+      digitalWrite(LED_BUILTIN,HIGH);
+      delay(1000);
+    }    
+  }
+
+  // Set LoRaWAN Class change CLASS_A or CLASS_C
+  lora.setDeviceClass(CLASS_A);
+
+  // Set Data Rate
+  lora.setDataRate(SF7BW125);
+
+  // set channel to random
+  lora.setChannel(MULTI);
+  
+  // Put OTAA Key and DevAddress here
+  lora.setDevEUI(devEui);
+  lora.setAppEUI(appEui);
+  lora.setAppKey(appKey);
+
+  //Join procedure
+  bool isJoined;
+  do {
+    #ifdef SERIAL_DEBUG
+      Serial.println("Joining...");
+    #endif 
+    isJoined = lora.join();
+    
+    //wait for 10s to try again
+    delay(5000);
+  }while(!isJoined);
+  
+  #ifdef SERIAL_DEBUG
+    Serial.println("Joined to network");
+  #endif
+
+}
+
+/////////////////////////// Loop //////////////////////////////////////////
+
+void loop() {  
+  
+  char sendBuffer[32];
+  char recvBuffer[255];
+  float readings[8];  
+  uint8_t recvStatus = 0;
+
+
+
+  // Take readings ////////////////////////////////
+
+  digitalWrite(PIN_EX_EN,LOW); //Turn on excitation
+  
+  //Get the bus/battery voltage
+  readings[0] = getBusVoltage();
+
+  //Read AD7794 chip temperature
+  readings[1] = adc.read(6);
+
+  //Read 6 ADC channels
+  for(int i=0; i < 6; i++){
+    readings[i+2] = adc.read(i);
+  }
+
+  digitalWrite(PIN_EX_EN,HIGH); //Turn off excitation
+  
+
+  // Send out the readings ///////////////////////
+  
+  //Pack it up...
+  memcpy(sendBuffer, readings, sizeof(readings)); 
+
+  //...and ship it out  
+  lora.sendUplink(sendBuffer, sizeof(sendBuffer), 0,1);  //This function has changed to include the mport arg
+                                                         //in the 1.5+ Version of the library. 
+                                                         
+  //lora.sendUplink(sendBuffer, sizeof(sendBuffer), 0);  //Earlier library versions
+  
+  // Check Lora RX
+  lora.update();
+
+
+
+  // Get reply and parse /////////////////////
+
+  recvStatus = lora.readData(recvBuffer);    
+     
+  if(recvStatus) {
+    parseDownlink(recvBuffer);
+    
+    //Blink LED to show we got a downlink
+    digitalWrite(PIN_BLUE_LED,LOW);
+    delay(500);
+    digitalWrite(PIN_BLUE_LED,HIGH);
+  }
+
+
+  // Timing & Sleep Code //////////////////////
+
+  nextAlarmSec = (nextAlarmSec + (sleepInterval % 60)) % 60;
+  nextAlarmMin = (nextAlarmMin + (sleepInterval / 60)) % 60;
+
+  #ifdef SERIAL_DEBUG    
+    Serial.print("nextAlarmSec: ");
+    Serial.println(nextAlarmSec);
+    Serial.print("nextAlarmMin: ");
+    Serial.println(nextAlarmMin);
+  #endif  
+
+  if(sleepInterval <= 60){
+    rtc.setAlarmSeconds(nextAlarmSec);    
+    rtc.enableAlarm(rtc.MATCH_SS); 
+  }else{
+    rtc.setAlarmSeconds(nextAlarmSec);  
+    rtc.setAlarmMinutes(nextAlarmMin);
+    rtc.enableAlarm(rtc.MATCH_MMSS);
+  }
+  
+  rtc.attachInterrupt(rtcAlarm);
+
+  
+  digitalWrite(LED_BUILTIN,LOW); 
+
+  #ifdef SERIAL_DEBUG
+    Serial.println(F("Going to (Fake) sleep now"));
+    delay(sleepInterval * 1000);
+    Serial.println(F("Good morning!"));
+  #else
+    RFM_Sleep();
+    //rtc.standbyMode(); 
+    goToSleep();
+  #endif
+  
+  digitalWrite(LED_BUILTIN,HIGH); //Waste of power, but this is just an example 
+    
+}
+
+////////////////////////// Functions /////////////////////////////////////////
+
+// Parse Any Downlink Messages, the BeeLan library doesn't seem to acknowledge 
+// ports so I am using a simple command byte scheme where the first byte in the
+// payload is a the "command". 
+// This is just to demonstrate the concept
+void parseDownlink(char * buf){
+  
+  //Struct to represent simple message
+  struct dlMessage_t{
+    uint8_t   cmd;
+    uint8_t   payload[32]; //Need to look into actual appropriate max payload size
+  } message;
+
+  memset(message.payload,0,sizeof(message.payload));  
+  memcpy(&message, buf, sizeof(buf));
+
+  //Debug info
+  #ifdef SERIAL_DEBUG
+    Serial.print(F("Message received [ "));
+    Serial.print(F("cmd: "));
+    Serial.print(message.cmd,HEX);
+    Serial.print(F(" Payload: "));
+    for(int i=0;i<sizeof(message.payload);i++){
+      Serial.print(message.payload[i],HEX);
+      Serial.print(' ');
+    }
+    Serial.println(" ]");
+  #endif
+
+
+  // We only have one command for now, but more can be added
+  // easily with additional if() statements.
+
+  // Set the transmit interval
+  if(message.cmd == DLCMD_SET_RATE){
+    
+    //We got it! 
+    digitalWrite(PIN_BLUE_LED,LOW);
+    delay(1000);
+    digitalWrite(PIN_BLUE_LED,HIGH);
+    
+    uint16_t arg1; 
+    arg1 =  message.payload[1] | message.payload[0] << 8 ;   
+    
+
+    #ifdef SERIAL_DEBUG
+      Serial.print("arg1 = ");
+      Serial.println(arg1,BIN);
+    #endif
+ 
+    //TODO: Set sleep interval here 
+    if((arg1 >= MIN_INTERVAL) && (arg1 < MAX_INTERVAL)){
+      sleepInterval = arg1;
+
+      //Just re-sync these whenever we change the interval to avoid timing issues
+      nextAlarmSec = rtc.getSeconds();
+      nextAlarmMin = rtc.getMinutes();
+      
+      #ifdef SERIAL_DEBUG
+        Serial.print(F("Set inteval to "));
+        Serial.println(arg1);
+      #endif
+    }
+    
+  }else{
+    //Unknown command
+    #ifdef SERIAL_DEBUG
+      Serial.println(F("Unknown command"));
+    #endif
+    
+  }
+  
+}
+
+
+// Trusted sleep code. You could also just use rtc.standbyMode(), but in the past it
+// would cause lockups due to not disabling SysTick. I believe that has been fixed 
+// but since sleep is so critical, I like to use this explicit block of code. 
+void goToSleep(){
+  
+  USBDevice.detach();
+
+  //Microchip recomended code to disable SysTick interupt first
+  SysTick->CTRL &= ~SysTick_CTRL_TICKINT_Msk;
+
+  //Go to sleep
+  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+  __DSB();
+  __WFI();
+  //Code starts here after waking
+
+  //Reenable systick interrupt
+  SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;
+  
+  //USBDevice.init();
+  USBDevice.attach(); 
+  
+}
+
+// The Beelan-LoRaWAN libray doesn't have a method to put the radio to sleep
+// so we'll do it manually
+void RFM_Sleep(){
+
+    //Borrowed from RadioHead library
+    const uint8_t RH_RF95_MODE_SLEEP      = 0x00;
+    const uint8_t RH_RF95_REG_01_OP_MODE  = 0x01;
+    const uint8_t RH_SPI_WRITE_MASK       = 0x80;
+    
+    //Set NSS pin Low to start communication
+    digitalWrite(PIN_RFM_CS,LOW);
+
+    //Send Addres with MSB 1 to make it a write command
+    SPI.transfer(RH_RF95_REG_01_OP_MODE | RH_SPI_WRITE_MASK);
+    //Send Data
+    SPI.transfer(RH_RF95_MODE_SLEEP);
+
+    //Set NSS pin High to end communication
+    digitalWrite(PIN_RFM_CS,HIGH);
+}
+
+
+// Return the battery voltage
+float getBusVoltage(){
+  return ((analogRead(BUS_V_PIN) * 3.3) / 4096) *2;
+}
+
+// RTC Alarm Match ISR
+void rtcAlarm(){
+  //Do nothing for now
+  
+}
+
+/***********************************************************************************
+Example Javascript Decode/Encode functions for Chirpstack. This will also work for
+TTN but you have to change the Decode signature slightly to Decoder(bytes, port)
+  
+From https://www.thethingsnetwork.org/forum/t/decode-float-sent-by-lopy-as-node/8757/2
+Based on https://stackoverflow.com/a/37471538 by Ilya Bursov
+
+************************************************************************************
+
+
+// Helper that converts incoming raw byte back to floats 
+
+function bytesToFloat(bytes) {
+  // JavaScript bitwise operators yield a 32 bits integer, not a float.
+  // Assume LSB (least significant byte first).
+  var bits = bytes[3]<<24 | bytes[2]<<16 | bytes[1]<<8 | bytes[0];
+  var sign = (bits>>>31 === 0) ? 1.0 : -1.0;
+  var e = bits>>>23 & 0xff;
+  var m = (e === 0) ? (bits & 0x7fffff)<<1 : (bits & 0x7fffff) | 0x800000;
+  var f = sign * m * Math.pow(2, e - 150);
+  return f;
+  
+  //The “bias” value, which is documented 5 to be 127 for 32 bits single-precision IEEE-754 floating point, has been replaced
+  //by 150 in m * Math.pow(2, e - 150). This is effectively m × 2-23 × 2e-127 a.k.a. (m / 223) × 2e-127, and is a nice optimization
+  //to get the 24th implicit leading bit in the “mantissa”.
+}  
+
+
+// Decode for Battery, temparature and 6 AIN channels from AD7794 
+// !!! Use Decoder(bytes, port) for The Things Network
+
+function Decode(fPort, bytes){
+  var payloadObj = {"BattVoltage":"",
+                    "Temperature":"",
+                    "Ch_0":"",
+                    "Ch_1":"",
+                    "Ch_2":"",
+                    "Ch_3":"",
+                    "Ch_4":"",
+                    "Ch_5":""};
+  
+  //var floats = new Float32Array(2);
+  var b = 0;
+  payloadObj.BattVoltage = bytesToFloat(bytes.slice(b, b+=4));
+  payloadObj.Temperature = bytesToFloat(bytes.slice(b, b+=4));
+  payloadObj.Ch_0 = bytesToFloat(bytes.slice(b, b+=4));
+  payloadObj.Ch_1 = bytesToFloat(bytes.slice(b, b+=4));
+  payloadObj.Ch_2 = bytesToFloat(bytes.slice(b, b+=4));
+  payloadObj.Ch_3 = bytesToFloat(bytes.slice(b, b+=4));
+  payloadObj.Ch_4 = bytesToFloat(bytes.slice(b, b+=4));
+  payloadObj.Ch_5 = bytesToFloat(bytes.slice(b, b+=4));
+  
+  return payloadObj;
+}
+
+
+// Javascript Encode function for sending a 1 byte command with a single uint16 argument 
+
+function Encode(fPort, obj) {
+  var bytes = [];
+  
+  bytes[0] = obj.command & 0xFF;
+  
+  bytes[1] = (obj.arg1 >> 8) & 0xFF;
+  bytes[2] = obj.arg1 & 0xFF;
+  
+
+  return bytes; 
+}
+
+*****************************************************************************************/
---
+++ JLogger_601_Test_All/JLogger_601_Test_All.ino
@@ -0,0 +1,404 @@
+/*******************************************************************************************
+ * This example sketch can be used to test hardware functionality of JLogger-601. 
+ * 
+ * Jaimy Juliano
+ * NHB Sytems
+ * October 20th, 2020
+ * 
+ *******************************************************************************************/
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Defines for optional components - COMMENT OUT ANYTHING NOT INSTALLED BEFORE UPLOADING TO LOGGER!
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+//#define HAS_FRAM_CHIP         // TODO: Refactor as specific FRAM chip to avoid confusion
+                                // Use this if an FM24CL04B FRAM IC has been installed on
+                                // the SOIC pad on the bottom of the board
+
+//#define HAS_RFM95_RADIO       // If you uncomment a radio option, you must have the Radiohead library installed.
+//#define HAS_RFM69_RADIO
+#define RFM_FREQUENCY  915.00   // If testing radio, make sure this frequency is okay for your region
+
+#define HAS_24AA025E64_CHIP     // Included on all boards now
+
+//#define HAS_ATECC608A         // Currently unsuported
+
+//#define TEST_RADIOHEAD        // Not implemented
+
+//#define TEST_LORAWAN          // Not implemented
+
+#define TEST_FLASH  
+#define TEST_LEDS
+#define TEST_SW3V
+
+// Currently the AD7794 and the 2.5V excitation regulator are always tested
+
+///////////////////////////////////  Includes //////////////////////////////////////////////////////
+
+#include <SPI.h>
+#include <Wire.h>
+#include <RTCZero.h>
+#include "NHB_AD7794.h"
+
+
+/////////////////////////////// Conditional Includes ///////////////////////////////////////////////
+#ifdef TEST_FLASH
+  #include <SdFat.h>
+  #include <Adafruit_SPIFlash.h>
+#endif
+
+
+// For now I'll just use the RadioHead library to make sure the radio can
+// be initialized. Testing with LoRaWAN is too much overhead for a simple
+// hardware test. May create a standalone function to test communication 
+// with the radio in the future, just to be more self contained.
+#ifdef HAS_RFM95_RADIO
+  #include <RH_RF95.h>
+#endif
+
+#ifdef HAS_RFM69_RADIO
+  #include <RH_RF69.h>
+#endif
+  
+
+#include <FRAM_MB85RC_I2C.h> 
+
+#ifdef HAS_24AA025E64_CHIP
+  //#include "Microchip24AA02E.h" //Don't need this, can just use extEEPROM library
+  #include <extEEPROM.h>
+#endif
+
+
+////////////////////////////// Globals /////////////////////////////////////////////////
+
+const uint32_t AlarmSeconds = 10;
+
+AD7794 adc(PIN_AD7794_CS,4000000, 2.50);    // Create the ADC object
+
+RTCZero rtc;
+SPISettings spiSettings;
+
+//mac64 eui;
+uint8_t eui[8];
+
+double readings[6]; //6 channels
+double adcTemperature;
+
+
+///////////////////////////// Conditional globals ///////////////////////////////////////
+
+#ifdef HAS_24AA025E64_CHIP
+  extEEPROM eep(kbits_2, 1, 16);         //device size, number of devices, page size
+#endif
+
+//RFMXX
+#ifdef HAS_RFM95_RADIO
+  RH_RF95 driver(PIN_RFM_CS,PIN_RFM_IRQ);
+  //RHReliableDatagram manager(driver, NODE_ADDRESS); 
+#endif
+
+#ifdef HAS_RFM69_RADIO
+  RH_RF69 driver(PIN_RFM_CS,PIN_RFM_IRQ);
+  //RHReliableDatagram manager(driver, NODE_ADDRESS);
+#endif
+
+
+//FRAM (FM24CL04B right now)
+#ifdef HAS_FRAM_CHIP
+  const uint8_t framAddress = 0x50;
+  const uint16_t framDensity = 4; //4k (4, 16, 64, 128, 256, 512, 1024)
+  const uint8_t wpPin = 42; //Don't care, not using
+  const bool wpEnabled = false;
+  FRAM_MB85RC_I2C framMem(framAddress, wpEnabled, wpPin, framDensity);
+#endif
+
+#ifdef TEST_FLASH
+  Adafruit_FlashTransport_SPI flashTransport(PIN_FLASH_CS, &SPI);
+  Adafruit_SPIFlash flash(&flashTransport);
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// setup() - most of the tests are actually run here
+////////////////////////////////////////////////////////////////////////////////////////////
+
+void setup() {
+
+  Serial.begin(115200);
+  while (!Serial);
+  
+
+  //initLoggerPins();
+  Wire.begin();
+  
+  rtc.begin();
+
+  
+
+/////////////////////////// Test LEDs //////////////////////////////////
+#ifdef TEST_LEDS
+  Serial.println(F("** Testing LEDs **"));
+  
+  for(int i = 0; i < 10; i++){
+    digitalWrite(PIN_BLUE_LED, HIGH);   
+    digitalWrite(PIN_LED_RXL, LOW);
+    digitalWrite(PIN_EX_EN,HIGH);
+    digitalWrite(PIN_LED_TXL, HIGH);
+    digitalWrite(PIN_LED_13,HIGH);
+    delay(100); // wait a bit
+    
+    digitalWrite(PIN_BLUE_LED, LOW);   
+    digitalWrite(PIN_LED_RXL, HIGH);
+    digitalWrite(PIN_EX_EN,LOW);
+    digitalWrite(PIN_LED_TXL, LOW);
+    digitalWrite(PIN_LED_13,LOW);
+    delay(100); // wait a bit
+  }
+  
+  digitalWrite(PIN_BLUE_LED, HIGH); //Turn off blue
+  
+  Serial.println();
+  
+#endif
+
+
+//////////////////////// Test Switched 3.3V (MOSFET) Output //////////////
+#ifdef TEST_SW3V
+  Serial.println(F("*** Testing Switched 3.3V Supply ***"));
+  Serial.println(F("Switched 3.3V pin will turn on for 2 seconds"));
+  digitalWrite(PIN_SW_3V, LOW);
+  delay(2000);
+  digitalWrite(PIN_SW_3V,HIGH);
+  Serial.println(F("Turning off"));
+  Serial.println();
+#endif
+
+
+////////////////////// Test Radio Inititialization /////////////////////////  
+#if defined(HAS_RFM95_RADIO) || defined(HAS_RFM69_RADIO)
+    
+  Serial.println(F("*** Testing RFMxx Radio ***"));
+  
+  if (!driver.init()){
+    //#ifdef SERIAL_DEBUG
+      Serial.println("init failed!");
+    //#endif
+  }else{
+    Serial.println("Radio initialized");
+    if (!driver.setFrequency(RFM_FREQUENCY)){
+    //#ifdef SERIAL_DEBUG
+      Serial.println("setFrequency failed");
+    //#endif
+    }else{
+      Serial.print("Set frequency to ");
+      Serial.print(RFM_FREQUENCY);
+      Serial.println(" MHz");
+    }
+  } 
+
+  Serial.println();
+
+  //TODO: Setup RHReliableDatagram and send out a message here
+  
+#endif
+
+
+#ifdef HAS_FRAM_CHIP  
+  framMem.begin();
+  //TODO: Do test
+#endif
+
+
+///////////////////////// Test EUI-64 Chip /////////////////////////////
+#ifdef HAS_24AA025E64_CHIP
+  Serial.println(F("** Testing 24AA025E64 **"));
+
+  uint8_t eepStatus = eep.begin(eep.twiClock400kHz);   //go fast!
+  if (eepStatus) {
+    Serial.print(F("extEEPROM.begin() failed, status = "));
+    Serial.println(eepStatus);
+    //while (1);
+  }
+  
+  //MacReader.readMac64(eui);
+  eep.read(0xf8,eui,8);
+  Serial.print("EUI64: ");
+  for (int i = 0; i < sizeof(eui) / sizeof(eui[0]); i++) {
+    Serial.print("0x");
+    Serial.print(eui[i],HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+
+  //Now convert to string
+  char devEuiStr[16];
+  char buf[3];
+  
+  for(int i=0;i<8;i++){
+    sprintf(buf, "%02x",eui[i]);
+    //try some garbage
+    devEuiStr[i*2] = buf[0];
+    devEuiStr[i*2 + 1] = buf[1];
+  }
+  devEuiStr[16] = 0; //null terminate
+  
+  Serial.print("As string: ");
+  
+  Serial.print(devEuiStr);
+  Serial.println();
+  Serial.println();
+
+  delay(1000);
+#endif
+
+///////////////////////////// Test Flash ////////////////////////////////
+
+#ifdef TEST_FLASH
+  //From Adafruit example
+  flash.begin();
+  
+  Serial.println(F("** Testing Flash storage IC **"));
+  Serial.print("JEDEC ID: "); Serial.println(flash.getJEDECID(), HEX);
+  Serial.print("Flash size: "); Serial.println(flash.size());
+
+  uint8_t b1, b2, b3;
+  uint32_t JEDEC = flash.getJEDECID();
+  //uint16_t ManID = flash.getManID();
+  b1 = (JEDEC >> 16);
+  b2 = (JEDEC >> 8);
+  b3 = (JEDEC >> 0);
+
+  Serial.print(b1,HEX);
+  Serial.print(b2,HEX);
+  Serial.print(b3,HEX);
+
+  Serial.println();
+  Serial.println();
+
+  delay(1000);
+#endif
+
+///////////////////////// Test AD7794 //////////////////////////////////
+
+  adc.begin();
+  adc.setUpdateRate(16.6);
+  
+  
+  for(int i=0; i < 6; i++){
+    adc.setBipolar(i,true);
+    //delay(2);
+    adc.setGain(i, 128);
+    //delay(2);
+    adc.setEnabled(i,true);
+    //delay(2);
+  }
+
+  //Read all 6 channels and spit out on serial
+  Serial.println(F("** Test 24 bit ADC Readings ***"));
+
+  Serial.println(F("Turning on excitation voltage"));
+  digitalWrite(PIN_EX_EN,LOW); //Turn on excitation
+
+  Serial.println(F("Reading all 6 channels"));
+  for(int i=0; i < 6; i++){
+    readings[i] = adc.read(i);
+    Serial.print("Ch ");
+    Serial.print(i);
+    Serial.print(": ");    
+    Serial.println(readings[i],DEC);    
+  }
+  //Serial.println();
+
+  //Read the onboard temperature sensor too 
+  // (Reading channel 6 returns temperature, but it may need an offset calibration)
+  adcTemperature = adc.read(6); 
+  Serial.print("And the temperature: ");
+  Serial.print(adcTemperature); 
+  Serial.println(" C"); 
+
+  Serial.println(F("Turning off excitation"));
+  digitalWrite(PIN_EX_EN,HIGH); //Turn off excitation
+
+  Serial.println();
+
+
+/////////////////////////// Test User Button and Go To sleep ////////////////////////////////
+
+
+  Serial.println(F("Press user button to go to sleep (USB wil be disconnected)"));
+  while (digitalRead(PIN_BUTTON) == HIGH) delay(10);    //wait for button push
+  
+  
+  Serial.println("Going to sleep now, good night");
+
+#if defined(HAS_RFM95_RADIO) || defined(HAS_RFM69_RADIO)      
+  driver.sleep();
+#endif
+
+  delay(1000);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// loop() - Do nothing except sleep, wakeup, go back to sleep. 
+// This is where I test the sleep current draw
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+void loop() {
+
+  //Serial.print(F("SLEEP DISABLED WHILE WORKING"));
+  //while(1){}; //Temporary, will fiddle with sleep later
+
+  //TODO: Maybe I should have it sleep until the button is pushed again?
+  
+  
+  static uint32_t nextAlarm = rtc.getSeconds(); 
+
+
+  nextAlarm = (nextAlarm + AlarmSeconds) % 60;
+  rtc.setAlarmSeconds(nextAlarm);
+  rtc.enableAlarm(rtc.MATCH_SS);
+  rtc.attachInterrupt(rtcAlarm);
+  
+  //Serial.println("Going to sleep");
+  digitalWrite(PIN_BLUE_LED,HIGH);
+
+
+  digitalWrite(PIN_LED_TXL, HIGH); //Make sure TX led is off before going to sleep
+  
+  //rtc.standbyMode();  // <-- This should work fine too
+  goToSleep();
+  
+  
+  digitalWrite(PIN_BLUE_LED,LOW);
+    
+  
+  delay(2000); 
+
+}
+
+
+//This may need updating to get USB wakeup working right - not sure?
+void goToSleep(){
+  
+  USBDevice.detach();
+  
+  //Disable SysTick interupt
+  SysTick->CTRL &= ~SysTick_CTRL_TICKINT_Msk;
+
+  //Go to sleep
+  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+  __DSB();
+  __WFI();
+  //Code starts here after waking
+
+  //Reenable systick interrupt
+  SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;
+ 
+  USBDevice.attach();  
+  
+}
+
+//RTC Alarm Match ISR
+void rtcAlarm(){
+  //Do nothing for now  
+}
